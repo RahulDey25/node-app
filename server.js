a30f2691bf764abe221d72ed7859cf1f1a973c34
Rahul <deyr279@gmail.com>
2024-02-07 21:36:33 +0530

    added student endpoint

--- server.js
+++ server.js
@@ -7,4 +7,18 @@ app.get('/',(req,res) => {
     res.status(200).json({name: 'node-app-new'})
 })
 
+app.get('/student',(req,res)=>{
+    res.status(200).json([{
+        name:"Rahul",
+        age:23,
+        city:'Mumbai'
+    },
+{
+    name:"Rajni",
+    age:67,
+    city:'Chennai'
+}
+])
+})
+
 app.listen(3000)
